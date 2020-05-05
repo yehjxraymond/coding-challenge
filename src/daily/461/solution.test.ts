12ee5e4c6a6dbcdda149c8160e5b0c21ae1dbd04
Raymond Yeh <ray@geek.sg>
2020-05-05 21:38:21 +0800

    fix:comments

--- src/daily/461/solution.test.ts
+++ src/daily/461/solution.test.ts
@@ -9,8 +9,8 @@ const factorial = (num: number): number => {
 const countTraversal = (n: number, m: number) => {
   // Given a start node and end node, the total number of right and down
   // actions will be the same, just in different permutations
-  // Number of right = col = m
-  // Number of down = row = n
+  // Number of right = col - 1 = m - 1
+  // Number of down = row - 1 = n - 1
   // Ie for a given 2x2 matrix, it will be 1 right and 1 down
   // Ie for a given 5x5 matrix, it will be 4 right and 4 down
   // So we are really just coming up with the number of permutations of similar objects
